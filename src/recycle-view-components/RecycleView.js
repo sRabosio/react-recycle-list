15e2184d1c253fc2456f48b9ff23bfdbf17169fc
INXPIRE\s.rabosio <rabosio@it-sol.it>
2023-02-16 18:01:45 +0100

    full functional recycling

--- src/recycle-view-components/RecycleView.js
+++ src/recycle-view-components/RecycleView.js
@@ -31,6 +31,7 @@ const testData = new Array(200).fill("sono un pollo")
 export const RecycleList = ({ListItem, itemHeight}) => {
     const listContainer = useRef(null)
     let [items, setItems] = useState([])
+    let [scrollTarget, setscrollTarget] = useState(null)
     const [y, setY] = useState(0)
     dataObj.dataArray = testData
 
@@ -73,48 +74,79 @@ export const RecycleList = ({ListItem, itemHeight}) => {
         return newItems
     }
 
-    //moving components
-    const onScroll = e=>{
-        console.log(items.length);
-        //highest & lowest pixel
-        const yTop = e.currentTarget.scrollTop 
-        const yBottom = e.currentTarget.scrollTop+listContainer.current.clientHeight
-        const yCenter = e.currentTarget.scrollTop+listContainer.current.clientHeight/2
-        const lowestItem = items.at(-1).ref.current
-        const highestItem = items.at(0).ref.current
-        const newItemArray = [...items]
-        const direction = yCenter<y ? "top" : "bottom"
-        let newItem = null;
-        let data = null;
+    const scrollDirection = posProp=>{
+        return posProp.yCenter<y ? "top" : "bottom"
+    }
+    const goDown = posProp=>{
+        return posProp.yTop > posProp.highestItem.offsetTop+posProp.highestItem.clientHeight && scrollDirection(posProp) === "bottom"
+    }
+    const goUp = posProp=>{
+        return posProp.yBottom < posProp.lowestItem.offsetTop-posProp.lowestItem.clientHeight && scrollDirection(posProp) === "top"
+    }
 
-        //TODO: full recycling
-
-        if(yTop > highestItem.offsetTop+lowestItem.clientHeight && direction === "bottom"){
-            data = dataObj.getNextData();
-            if(!data) return
-            newItem = newItemArray.shift()
-            newItem.data = data
-            newItem.top = newItemArray.at(-1).top + itemHeight
-            newItem.ref.current.style.top = newItem.top;
-            newItemArray.push(newItem)
-            setItems(newItemArray)
-        }else if(yBottom < lowestItem.offsetTop-lowestItem.clientHeight && direction === "top"){
-            data = dataObj.getPrevData()
-            if(!data) return
-            newItem = newItemArray.pop()
-            newItem.data = data
-            newItem.top = newItemArray.at(0).top - itemHeight
-            newItem.ref.current.style.top = newItem.top
-            newItemArray.unshift(newItem)
-            setItems(newItemArray)
+    const getPosProp = itemArray=>{
+        if(!scrollTarget) return
+        return {
+            yTop: scrollTarget.scrollTop,
+            yBottom: scrollTarget.scrollTop+listContainer.current.clientHeight,
+            yCenter: scrollTarget.scrollTop+listContainer.current.clientHeight/2,
+            lowestItem: itemArray.at(-1).ref.current,
+            highestItem: itemArray.at(0).ref.current,
         }
-        //console.log("direction", {direction, y, yCenter});
-        setY(yCenter)
-
     }
 
     
+    //moving components
+    const onScroll = ()=>{
+        if(!scrollTarget) return
+        console.log("number of items: ",items.length);
+        //highest & lowest pixel
+        const posProp = getPosProp(items)
+        if(goDown(posProp))
+            pushdown(posProp, [...items])
+        else if(goUp(posProp))
+            pushup(posProp, [...items])
+        setY(posProp.yCenter)
+        setscrollTarget(null)
+    }
+    
+    useEffect(onScroll, [scrollTarget])
+    
+    
+    
+    const pushdown = (posProp, newItemArray)=>{
+        let newItem = null;
+        const data = dataObj.getNextData();
+        if(!data) return
+        newItem = newItemArray.shift()
+        newItem.data = data
+        newItem.top = newItemArray.at(-1).top + itemHeight
+        newItem.ref.current.style.top = newItem.top;
+        newItemArray.push(newItem)
+        
+        posProp = getPosProp(newItemArray)
 
+        setItems(newItemArray)
+        if(goDown(posProp)) pushdown(posProp, [...newItemArray])
+    }
+
+    const pushup = (posProp, newItemArray)=>{
+        let newItem = null;
+        const data = dataObj.getPrevData()
+        if(!data) return
+        newItem = newItemArray.pop()
+        newItem.data = data
+        newItem.top = newItemArray.at(0).top - itemHeight
+        newItem.ref.current.style.top = newItem.top
+        newItemArray.unshift(newItem)
+
+        posProp = getPosProp(newItemArray)
+
+
+        setItems(newItemArray)
+        if(goUp(posProp)) pushup(posProp, [...newItemArray])
+    }
+    
     const getRatio = ()=>{
         return (listContainer.current.clientHeight/itemHeight)+3;
     }
@@ -129,7 +161,10 @@ export const RecycleList = ({ListItem, itemHeight}) => {
          overflowY: "scroll",
           height: "100%",
            minWidth: "100%"}}
-           onScroll={onScroll}>
+           onScroll={e=>{
+            if(scrollTarget) return
+            setscrollTarget(e.currentTarget)
+           }}>
             {items.map((value, index)=>{
                 //map fa una copia dell'array quindi per settare ref devo farmi dare il puntatore direttamente dall'items originale
                 const ref = items[index].ref;
